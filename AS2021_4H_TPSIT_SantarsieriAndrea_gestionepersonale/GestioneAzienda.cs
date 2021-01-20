ebf0988e9f0b2a81f25dda637064be6b0ca7ddd4
andreasantarsieri <andrea.santarsieri@gmail.com>
2021-01-20 11:07:35 +0100

    Gestione personale

---
+++ AS2021_4H_TPSIT_SantarsieriAndrea_gestionepersonale/GestioneAzienda.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AS2021_4H_TPSIT_SantarsieriAndrea_gestionepersonale
+{
+    class GestioneAzienda
+    {
+        string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                _nome = value;
+            }
+        }
+        string _cognome;
+        public string Cognome
+        {
+            get
+            {
+                return _cognome;
+            }
+            set
+            {
+                _cognome = value;
+            }
+        }
+        int _eta;
+        public int Eta
+        {
+            get
+            {
+                return _eta;
+            }
+            set
+            {
+                _eta = value;
+            }
+        }
+        string _reparto;
+        public string Reparto
+        {
+            get
+            {
+                return _reparto;
+            }
+            set
+            {
+                _reparto = value;
+            }
+        }
+
+        public GestioneAzienda(string _nome, string _cognome , int _eta, string _reparto)
+        {
+            _nome = Nome;
+            _cognome = Cognome;
+            _eta = Eta;
+            _reparto = Reparto;
+        }
+        public string VisualizzaDati()
+        {
+            return $"Il dipendente di nome {Nome} e di cognome {Cognome} ha {Eta} anni e lavora nel reparto {Reparto}.";
+        }
+        public string CancellaDati()
+        {
+            _nome = ".";
+            _cognome = ".";
+            _eta = 0;
+            _reparto = ".";
+            return $"Il dipendente di nome {Nome} e di cognome {Cognome} ha {Eta} anni e lavora nel reparto {Reparto}.";
+        }
+        public string ModificaDati()
+        {
+            return $"Il dipendente di nome {Nome} e di cognome {Cognome} ha {Eta} anni e lavora nel reparto {Reparto}.";
+        }
+
+
+    }
+}
---
+++ AS2021_4H_TPSIT_SantarsieriAndrea_gestionepersonale/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AS2021_4H_TPSIT_SantarsieriAndrea_gestionepersonale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            GestioneAzienda utente = new GestioneAzienda("Gino", "Rossi", 24, "Magazzino1");
+
+            utente.Nome = "Gino";
+            utente.Cognome = "Rossi";
+            utente.Eta = 24;
+            utente.Reparto = "Magazzino1";
+
+            Console.WriteLine(utente.VisualizzaDati());
+
+            i++;
+            if(i == 1)
+            {
+                utente.Nome = "Mario";
+                utente.Cognome = "Verde";
+                utente.Eta = 44;
+                utente.Reparto = "Ufficio1";
+            }
+    
+            Console.WriteLine(utente.VisualizzaDati());
+            Console.WriteLine(utente.CancellaDati());
+            
+            
+
+
+        }
+    }
+}
